6f274f8aab15055a410e87d2a4f2cff0bac85356
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-08-05 20:58:29 -0600

    Added Tailwind

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
+    },
+  },
+  plugins: [],
+};
